4d9f680dc92fdaae237b65b6a7622990aa22aca8
Arfa Adnan <arfaadnan0@gmail.com>
2023-09-11 21:19:01 +0500

    add two files

---
+++ app.js
@@ -0,0 +1,264 @@
+// Chapter no 01
+// Question # 01
+var firstName="Arfa";
+var lastName="Adnan";
+var Email="123@gmail.com";
+var phoneNumber="1234567";
+var Password="123456789";
+// alert(firstName);
+// alert(lastName);
+// alert(Email);
+// alert(phoneNumber);
+// alert(Password);
+
+
+// Question # 02
+
+// alert("alert You're learning JavaScript!")
+
+// Question # 03
+
+// alert("Welcome To Java Script Assigment # 01")
+
+// Chapter No 02
+// Question # 01 
+
+// var companyName
+
+// Question # 02
+
+var companyName
+companyName="apple"
+// document.write(companyName);
+
+// Question # 03
+var teamName
+teamName="myTeam";
+// alert(teamName)
+
+// Question # 04
+
+var bestMan = "Charlie";
+// document.write(bestMan)
+
+// Chapter No 03
+// Question # 01
+// var caseQty;
+// Question # 02
+// var caseQty=143;
+// caseQty=144;
+
+// Qusetion # 03
+
+var num="9";
+// Question # 04
+
+var sum;
+// sum=2+7;
+
+
+
+// Qusetion # 05
+var merchTotal;
+merchTotal=100;
+var shippingCharge;
+shippingCharge=10;
+var orderTotal=merchTotal+shippingCharge;
+// document.write(orderTotal)
+
+// Question # 06
+
+var num=2;
+var num2=3
+var sum=num+num2;
+// document.write(sum)
+ 
+// Chapter No 04
+
+// Question # 01
+
+var productCost;
+// productCost=3.45;
+
+// Question # 02
+var nameOfBand;
+// Question # 03
+var  legal_num=8;
+// Question # 04
+var firstName;
+firstName="Arfa";
+var lastName;
+lastName="Adnan";
+var fullName=firstName+" "+lastName;
+// document.write(fullName);
+
+// Question # 05
+
+// A letter, dollar sign($), or underscore (_) must make up the first character. 
+// A number cannot be the initial character. Any letter, number, or underscore 
+// can complete the variable name. You cannot use some characters, such as 
+// spaces, symbols, and punctuation.
+
+
+// Chapter no 05
+
+// Question # 01
+// The remainder operator % is also known as the modulo or modulus operator.
+// var remainder= 3%5;
+// console.log(remainder);
+
+// Question # 02
+
+// var num=20 % 6;
+// console.log(num);
+
+// Question # 03
+
+// var largeNum=1000 * 2000;
+// console.log(largeNum);
+
+// Question # 04
+// var number=10;
+// number=number-5;
+// console.log(number);
+
+// Question # 05
+
+// var dividend= 14;
+// var divisor=4;
+// var remainder=dividend % divisor;
+// console.log(remainder);
+
+
+// Question # 06
+// var num;
+// num=3;
+// var num2;
+// num2=5;
+// var result=num*num2;
+// alert("The answer of Multiplication is "+result)
+
+// *****************The End******************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
